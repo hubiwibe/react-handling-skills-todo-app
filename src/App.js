21f8a024043c06988c8fe9c0827fa78026e07a56
yungi jung <hubiwibe@gmail.com>
2020-04-27 15:36:33 +0900

    기능 구현 완료

--- src/App.js
+++ src/App.js
@@ -1,14 +1,67 @@
-import React from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import './App.css';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'Learn react basics',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: 'Try componenet styling',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: 'Create a Todo app',
+      checked: false,
+    },
+  ]);
+
+  // 고유값으로 사용될 id
+  // ref를 사용하여 변수 담기
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
+
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos],
+  );
+
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [todos],
+  );
+
   return (
     <TodoTemplate>
-      <TodoInsert />
-      <TodoList />
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
 }
